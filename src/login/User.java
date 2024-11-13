63ce2f1ed88a01a9fd3ee7a3cd0abea05d11adec
Le0-W-L <leandro.lanzoni0400@gmail.com>
2024-11-13 16:51:08 -0300

    Identificação dos Nós para realização do Grafo de Fluxo.

--- src/login/User.java
+++ src/login/User.java
@@ -6,38 +6,56 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
+    // Nó 01
     public Connection conectarBD() {
         Connection conn = null;
         try {
+            //Nó 02
             Class.forName("com.mysql.Driver.Manager").newInstance();
+
+            //Nó 03
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
             conn = DriverManager.getConnection(url);
         } 
         catch (Exception e) {
+            //Nó 04
         }
+        //Nó 05
         return conn;
     }
 
     public String nome = "";
     public boolean result = false;
 
+    //Nó 06
     public boolean verificarUsuario(String login, String senha) {
         String sql = "";
+
+        //Nó 07
         Connection conn = conectarBD();
-        // INSTRUCAO SQL
+
+        // INSTRUCAO SQL - Nó 08
         sql += "select none from usuarios ";
         sql += "where login = " + " ' " + login + " ' ";
         sql += " and senha = " + " ' " + senha + " '; ";
         try {
+            //Nó 09
             Statement st = conn.createStatement();
+
+            //Nó 10
             ResultSet rs = st.executeQuery(sql);
+
+            //Nó 11
             if (rs.next()) {
+                //Nó 12
                 result = true;
                 nome = rs.getString("nome");
             }
         } 
         catch (Exception e) {
+            //Nó 13
         }
+        //Nó 14
         return result;
     }
 } // fim da class
